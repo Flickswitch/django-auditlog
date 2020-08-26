8990cb7ce0b68e007012715be4f4d8301fc418b1
Jethro Muller <JethroMuller@gmail.com>
2020-08-26 14:00:38 +0200

    Use new Django JSONField instead of postgres contrib one

--- src/auditlog/models.py
+++ src/auditlog/models.py
@@ -3,11 +3,10 @@ import ast
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericRelation
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.postgres.fields import JSONField
 from django.contrib.postgres.indexes import GinIndex
 from django.core.exceptions import FieldDoesNotExist
 from django.db import models, DEFAULT_DB_ALIAS
-from django.db.models import QuerySet, Q
+from django.db.models import JSONField, QuerySet, Q
 from django.utils import formats, timezone
 from django.utils.encoding import smart_str
 from django.utils.translation import gettext_lazy as _
